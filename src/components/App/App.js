51a4f03b2ec8ed8bb54c459f173c5450cf35d495
Brian Kim <briandkim93@gmail.com>
2018-07-06 22:23:30 -0700

    Add render for SearchBar and SearchResultList

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+import SearchBar from '../../containers/SearchBar/SearchBar'
+import SearchResultList from '../../containers/SearchResultList/SearchResultList'
+
 class App extends Component {
   render() {
     return (
       <div className="app">
-        
+        <div>
+          <SearchBar />
+          <SearchResultList />
+        </div>
       </div>
     );
   }
