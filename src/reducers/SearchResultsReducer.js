9cfe714f013dc00f9f6d980ec671bd94ced7967c
Brian Kim <briandkim93@gmail.com>
2018-07-06 22:11:09 -0700

    Define SearchResultsReducer to handle FETCH_SEARCH_RESULTS actions to return the list of search results

---
+++ src/reducers/SearchResultsReducer.js
@@ -0,0 +1,11 @@
+import { FETCH_SEARCH_RESULTS } from '../actions/index';
+
+function SearchResultsReducer(state = null, action) {
+  switch (action.type) {
+    case FETCH_SEARCH_RESULTS:
+      return action.payload.data[1];
+  }
+  return state;
+}
+
+export default SearchResultsReducer;
