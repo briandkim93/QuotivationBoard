49d8213c08f2c24c1b4bcfe8005d3af1c52ac2a0
Brian Kim <briandkim93@gmail.com>
2018-07-07 11:15:03 -0700

    Convert SearchResultItem into a container to dispatch fetchQuotes action creator on user selection

--- src/containers/SearchResultItem/SearchResultItem.js
+++ src/containers/SearchResultItem/SearchResultItem.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import { fetchQuotes } from '../../actions/index';
 
 class SearchResultItem extends Component {
   render() {
     return (
-      <li>
+      <li onClick={() => this.props.fetchQuotes(this.props.term)}>
         {this.props.term}
       </li>
     );
   }
 };
 
-export default SearchResultItem;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({fetchQuotes}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchResultItem);
