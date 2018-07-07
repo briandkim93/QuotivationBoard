e64fd69104afc600249e5516936e9e7f0a0d2c6b
Brian Kim <briandkim93@gmail.com>
2018-07-06 22:33:55 -0700

    Move intial state return into default case

--- src/reducers/SearchResultsReducer.js
+++ src/reducers/SearchResultsReducer.js
@@ -4,8 +4,9 @@ function SearchResultsReducer(state = null, action) {
   switch (action.type) {
     case FETCH_SEARCH_RESULTS:
       return action.payload.data[1];
+    default:
+      return state;
   }
-  return state;
 }
 
 export default SearchResultsReducer;
