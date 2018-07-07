fae56c7ff605ab68aed0c9c4964596d9d7757d87
Brian Kim <briandkim93@gmail.com>
2018-07-07 11:19:27 -0700

    Define fetchQuotes action creator to handle Wikiquote API content request action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_SEARCH_RESULTS = 'FETCH_SEARCH_RESULTS';
+export const FETCH_QUOTES = 'FETCH_QUOTES';
 
 const baseURL = 'https://en.wikiquote.org/w/api.php?';
 
@@ -9,6 +10,16 @@ export function fetchSearchResults(query) {
   const request = axios.get(`${baseURL}${options}`);
   return {
     type: FETCH_SEARCH_RESULTS,
+    payload: request
+  };
+}
+
+export function fetchQuotes(term) {
+  const parsedTerm = term.replace(' ', '%20');
+  const options = `action=query&prop=revisions&rvprop=content&format=json&formatversion=2&titles=${parsedTerm}`;
+  const request = axios.get(`${baseURL}${options}`);
+  return {
+    type: FETCH_QUOTES,
     payload: request
   };
 }
