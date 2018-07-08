447f65165ce78bb02f715c7c956c27497c44d326
Brian Kim <briandkim93@gmail.com>
2018-07-08 00:05:52 -0700

    Modify rootReducer to bind DisplayLoaderReducer to application state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 
 import SearchResultsReducer from './SearchResultsReducer';
 import ModifySearchResultsReducer from './ModifySearchResultsReducer';
+import DisplayLoaderReducer from './DisplayLoaderReducer';
 import QuotesReducer from './QuotesReducer';
 
 const rootReducer = combineReducers({
   searchResults: SearchResultsReducer,
   filteredSearchResults: ModifySearchResultsReducer,
+  displayLoader: DisplayLoaderReducer,
   quotes: QuotesReducer
 });
 
