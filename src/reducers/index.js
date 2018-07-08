ef02be865aa4a86c45fdfece929218d5826711c6
Brian Kim <briandkim93@gmail.com>
2018-07-07 23:16:41 -0700

    Modify rootReducer to bind ModifySearchResultsReducer to application state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 
 import SearchResultsReducer from './SearchResultsReducer';
+import ModifySearchResultsReducer from './ModifySearchResultsReducer';
 import QuotesReducer from './QuotesReducer';
 
 const rootReducer = combineReducers({
   searchResults: SearchResultsReducer,
+  filteredSearchResults: ModifySearchResultsReducer,
   quotes: QuotesReducer
 });
 
