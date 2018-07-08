d0474b5df1fe07631a85b3f9ad150af91b8b1ff9
Brian Kim <briandkim93@gmail.com>
2018-07-07 23:06:31 -0700

    Define filterSearchResults action creator to use searchResults to filter usable Wikiquotes pages and define clearSearchResults action creator to handle reset of filteredSearchResults

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,34 @@
 import axios from 'axios';
 
 export const FETCH_SEARCH_RESULTS = 'FETCH_SEARCH_RESULTS';
+export const FILTER_SEARCH_RESULTS = 'FILTER_SEARCH_RESULTS';
+export const CLEAR_SEARCH_RESULTS = 'CLEAR_SEARCH_RESULTS';
 export const FETCH_QUOTES = 'FETCH_QUOTES';
 
 const baseURL = 'https://en.wikiquote.org/w/api.php?';
 
 export function fetchSearchResults(query) {
-  const options = `action=opensearch&search=${query}`;
+  const options = `action=opensearch&redirects=resolve&limit=3&search=${query}`;
   const request = axios.get(`${baseURL}${options}`);
   return {
     type: FETCH_SEARCH_RESULTS,
     payload: request
+  };
+}
+
+export function filterSearchResults(terms) {
+  const parsedTerms = terms.map(term => term.replace(' ', '%20'));
+  const requestLinks = parsedTerms.map(parsedTerm => `${baseURL}action=query&prop=revisions&rvprop=content&format=json&formatversion=2&titles=${parsedTerm}`);
+  const requests = Promise.all(requestLinks.map(requestLink => axios.get(requestLink)));
+  return {
+    type: FILTER_SEARCH_RESULTS,
+    payload: requests
+  };
+}
+
+export function clearSearchResults() {
+  return {
+    type: CLEAR_SEARCH_RESULTS
   };
 }
 
