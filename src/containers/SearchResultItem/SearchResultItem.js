f6a5ce114303ffeca8092d225eb011075b9901b7
Brian Kim <briandkim93@gmail.com>
2018-07-07 23:00:17 -0700

    Add functionality to dispatch clearSearchResults action creator on user selection

--- src/containers/SearchResultItem/SearchResultItem.js
+++ src/containers/SearchResultItem/SearchResultItem.js
@@ -3,11 +3,21 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 import { fetchQuotes } from '../../actions/index';
+import { clearSearchResults } from '../../actions/index';
 
 class SearchResultItem extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.handleItemClick = this.handleItemClick.bind(this);
+  }
+  handleItemClick() {
+    this.props.fetchQuotes(this.props.term);
+    this.props.clearSearchResults();
+  }
   render() {
     return (
-      <li onClick={() => this.props.fetchQuotes(this.props.term)}>
+      <li onClick={this.handleItemClick}>
         {this.props.term}
       </li>
     );
@@ -15,7 +25,7 @@ class SearchResultItem extends Component {
 };
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({fetchQuotes}, dispatch);
+  return bindActionCreators({fetchQuotes, clearSearchResults}, dispatch);
 }
 
 export default connect(null, mapDispatchToProps)(SearchResultItem);
