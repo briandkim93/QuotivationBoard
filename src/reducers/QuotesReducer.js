6f050109ea7becaea35f76ece866d9bab06b73ae
Brian Kim <briandkim93@gmail.com>
2018-07-07 23:11:02 -0700

    Add WikiquotesContentParser to parse quotes from content data of a Wikiquotes API request

--- src/reducers/QuotesReducer.js
+++ src/reducers/QuotesReducer.js
@@ -1,12 +1,17 @@
 import { FETCH_QUOTES } from '../actions/index';
 
+import WikiquotesContentParser from '../parsers/WikiquotesContentParser';
+
 function QuotesReducer(state = null, action) {
   switch (action.type) {
     case FETCH_QUOTES:
       const pages = action.payload.data.query.pages[0];
       const quotesTitle = pages.title;
       const quotesContent = pages.revisions[0].content;
-      return {quotesTitle, quotesContent};
+
+      const quotesParser = new WikiquotesContentParser(quotesContent);
+      const quotes = quotesParser.parse();
+      return {title: quotesTitle, quoteList: quotes};
     default:
       return state;
   }
