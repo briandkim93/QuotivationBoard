d1c92de87e7f52e3726189b5140f5947f0ce5378
Brian Kim <briandkim93@gmail.com>
2018-07-08 20:07:42 -0700

    Add redux-persist configurations to App

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReduxPromise from 'redux-promise';
-import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+import storage from 'redux-persist/lib/storage'
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
@@ -10,11 +13,22 @@ import registerServiceWorker from './registerServiceWorker';
 import App from './components/App/App';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['searchResults', 'filteredSearchResults', 'displayLoader']
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = createStore(persistedReducer, applyMiddleware(ReduxPromise));
+const persistor = persistStore(store);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>, 
   document.getElementById('root')
 );
