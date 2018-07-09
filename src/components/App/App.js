281354ecb29af7192e5c7341601aee4f39caaf9a
Brian Kim <briandkim93@gmail.com>
2018-07-08 20:20:33 -0700

    Terminate container import statements

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
-import SidePanel from '../SidePanel/SidePanel'
-import QuoteBoard from '../../containers/QuoteBoard/QuoteBoard'
+import SidePanel from '../SidePanel/SidePanel';
+import QuoteBoard from '../../containers/QuoteBoard/QuoteBoard';
 
 class App extends Component {
   render() {
