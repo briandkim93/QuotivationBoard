7d65fb8d8388656219d03a1985cfbe95dfe15504
Brian Kim <briandkim93@gmail.com>
2018-07-08 20:15:35 -0700

    Define and implement updateList method to track day changes and update quotes as such

--- src/containers/QuoteBoard/QuoteBoard.js
+++ src/containers/QuoteBoard/QuoteBoard.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import {bindActionCreators} from 'redux';
 import { connect } from 'react-redux';
 
+import QuoteBoardItem from '../QuoteBoardItem/QuoteBoardItem';
+
+import {fetchQuotes} from '../../actions/index';
+
 class QuoteBoard extends Component {
+  renderList() {
+    const quotesObjectList = this.props.quotes.map(
+      quoteObject => 
+        <QuoteBoardItem 
+          key={quoteObject.title}
+          quoteObject={quoteObject}
+        />
+    );
+    return quotesObjectList;
+  }
+  updateList(quoteObjectList) {
+    const date = new Date();
+    for (let i = 0; i < quoteObjectList.length; i += 1) {
+      if (quoteObjectList[i].dateSet !== date.toLocaleDateString()) {
+        quoteObjectList[i].current < quoteObjectList[i].total ? quoteObjectList[i].current += 1 : quoteObjectList[i].current = 0;
+        quoteObjectList[i].dateSet = date.toLocaleDateString()
+      }
+    }
+    return;
+  }
   render() {
+    this.updateList(this.props.quotes);
     return (
-      <div>
-        {this.props.quotes && this.props.quotes.quoteList}
+      <div className="quote-board col-8">
+        {this.props.quotes.length > 0 ? this.renderList() : ''}
       </div>
     );
   }
@@ -15,4 +41,8 @@ function mapStateToProps({quotes}) {
   return {quotes};
 }
 
-export default connect(mapStateToProps)(QuoteBoard);
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({fetchQuotes}, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(QuoteBoard);
