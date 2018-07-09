e89c46a7fffe92e0571be11b4bfc271979a74933
Brian Kim <briandkim93@gmail.com>
2018-07-08 20:12:34 -0700

    Add properties for tracking quote object information for use in daily updates

--- src/reducers/QuotesReducer.js
+++ src/reducers/QuotesReducer.js
@@ -2,16 +2,23 @@ import { FETCH_QUOTES } from '../actions/index';
 
 import WikiquotesContentParser from '../parsers/WikiquotesContentParser';
 
-function QuotesReducer(state = null, action) {
+function QuotesReducer(state = [], action) {
   switch (action.type) {
     case FETCH_QUOTES:
       const pages = action.payload.data.query.pages[0];
-      const quotesTitle = pages.title;
+      const title = pages.title;
       const quotesContent = pages.revisions[0].content;
 
       const quotesParser = new WikiquotesContentParser(quotesContent);
-      const quotes = quotesParser.parse();
-      return {title: quotesTitle, quoteList: quotes};
+      const quoteList = quotesParser.parse();
+
+      const date = new Date();
+      const dateSet = date.toLocaleDateString();
+      const existingTitles = state.map(quotesObject => quotesObject.title);
+
+      if (!existingTitles.includes(title)) {
+        return [...state, {title, quoteList, dateSet, current: 0, total: quoteList.length}];
+      }
     default:
       return state;
   }
