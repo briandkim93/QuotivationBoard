e67964dc98330012f3e51daedd1a218c7e74bb2b
Brian Kim <briandkim93@gmail.com>
2018-07-23 20:41:55 -0700

    Modify rootReducer to bind ToggleSidePanelReducer to application state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import { combineReducers } from 'redux';
 import SearchResultsReducer from './SearchResultsReducer';
 import ModifySearchResultsReducer from './ModifySearchResultsReducer';
 import DisplayLoaderReducer from './DisplayLoaderReducer';
+import ToggleSidePanelReducer from './ToggleSidePanelReducer';
 import QuotesReducer from './QuotesReducer';
 
 const rootReducer = combineReducers({
   searchResults: SearchResultsReducer,
   filteredSearchResults: ModifySearchResultsReducer,
   displayLoader: DisplayLoaderReducer,
+  sidePanelStatus: ToggleSidePanelReducer,
   quotes: QuotesReducer
 });
 
