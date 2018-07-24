55360a1ced40f1c64b375c27e7f3ed35784eff16
Brian Kim <briandkim93@gmail.com>
2018-07-23 20:46:24 -0700

    Use content parse results to determine if quotes are available

--- src/reducers/ModifySearchResultsReducer.js
+++ src/reducers/ModifySearchResultsReducer.js
@@ -1,18 +1,23 @@
 import { FILTER_SEARCH_RESULTS } from '../actions/index';
 import { CLEAR_SEARCH_RESULTS } from '../actions/index';
 
+import WikiquotesContentParser from '../parsers/WikiquotesContentParser';
+
 function ModifySearchResultsReducer(state = [], action) {
   switch (action.type) {
     case FILTER_SEARCH_RESULTS:
       const filteredSearchResults = action.payload.filter(
         result => {
           const quotesContent = result.data.query.pages[0].revisions[0].content;
-          return (quotesContent.includes("== Quotes ==") || quotesContent.includes("==Quotes=="))
+          const quotesParser = new WikiquotesContentParser(quotesContent);
+          let quoteList = quotesParser.parse();
+          quoteList = quoteList.filter(quote => quote.length <= 500);
+          return (quoteList.length > 0);
         }
       );
       const filteredTitles = filteredSearchResults.map(result => result.data.query.pages[0].title);
       if (filteredTitles.length > 0) { 
-        return filteredTitles;
+        return filteredTitles.slice(0, 5);
       } else {
         return ['No Results Found'];
       }
