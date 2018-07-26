118947f8de59bd26bed755c93fdd5d439a4c1326
Brian Kim <briandkim93@gmail.com>
2018-07-25 23:40:57 -0700

    Add functionality to clear search results

--- src/containers/SearchResultList/SearchResultList.js
+++ src/containers/SearchResultList/SearchResultList.js
@@ -3,6 +3,8 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 import './SearchResultList.css';
+import upPointer from './images/up-pointer.png';
+import { clearSearchResults } from '../../actions/index';
 
 import SearchResultListItem from '../SearchResultListItem/SearchResultListItem';
 
@@ -15,7 +17,17 @@ class SearchResultList extends Component {
   }
   render() {
     if (this.props.filteredSearchResults.length > 0) {
-      return <ul className="search-result-list">{this.renderList()}</ul>;
+      return (
+        <ul className="search-result-list">
+          {this.renderList()}
+          <li 
+            className="search-result-list-item clear-button"
+            onClick={this.props.clearSearchResults}
+          >
+            <img src={upPointer} />
+          </li>
+        </ul>
+      );
     }
     return <ul className="search-result-list"></ul>;
   }
@@ -25,4 +37,8 @@ function mapStateToProps({filteredSearchResults}) {
   return {filteredSearchResults};
 }
 
-export default connect(mapStateToProps)(SearchResultList);
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({clearSearchResults}, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchResultList);
